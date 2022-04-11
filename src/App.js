54c8dc97580537f893f637312ac2e24b1d2d95ee
Benedict <benedictryan80@gmail.com>
2022-04-11 23:40:15 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	useEffect(() => {
+		// Here we implement the main API at proxy in package.json in order to enable CORS
+		fetch('/jobs')
+			.then((resp) => {
+				console.log(resp);
+				console.log('======success=======');
+			})
+			.catch((err) => {
+				console.log('======failure=======');
+				console.log(err);
+			});
+	}, []);
+
+	return (
+		<div className="App">
+			<h1>Proxy Server usages in React</h1>
+		</div>
+	);
 }
 
 export default App;
